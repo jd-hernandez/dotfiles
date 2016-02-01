676ea74f0397e428fb9ffa1e17e7bec5f7fe0c4a
jhernandez <jhernandez@switchfly.com>
2016-02-01 17:56:27 -0600

    Added environ

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,9 @@
-{:dev {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                 [refactor-nrepl "2.0.0-SNAPSHOT"]
-                 [lein-pprint "1.1.1"]]
+{:dev {:plugins [[lein-environ "1.0.2"]
+                 [lein-pprint "1.1.1"]
+                 [refactor-nrepl "2.0.0-SNAPSHOT"]]
        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
-       :user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                        [refactor-nrepl "2.0.0-SNAPSHOT"]
-                        [lein-pprint "1.1.1"]]
-              :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+ :user {:plugins [[lein-environ "1.0.2"]
+                  [lein-pprint "1.1.1"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]}}
