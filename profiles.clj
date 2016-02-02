a507a7652e636450dabbb7d9bf870f382ee245cb
jhernandez <jhernandez@switchfly.com>
2016-02-02 14:29:11 -0600

    Added environ plugin.

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,7 @@
-{:dev {:plugins [[lein-environ "1.0.2"]
-                 [lein-pprint "1.1.1"]
+{:dev {:plugins [[lein-pprint "1.1.2"]
                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
- :user {:plugins [[lein-environ "1.0.2"]
-                  [lein-pprint "1.1.1"]
+ :user {:plugins [[lein-pprint "1.1.2"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
  :repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]}}
