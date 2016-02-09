1df2674910cf95e40dc519e9395bfb37b7f9f5df
jd-hernandez <jd.hernandez@gmail.com>
2016-02-09 13:11:29 -0600

    Updated refactor-nrepl to 2.2.0-SNAPSHOT

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
 {:dev {:plugins [[lein-pprint "1.1.2"]]}
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
         :plugins [[cider/cider-nrepl "0.11.0-snapshot"]]}
- :user {:plugins [[refactor-nrepl "2.0.0-SNAPSHOT"]
+ :user {:plugins [[refactor-nrepl "2.2.0-SNAPSHOT"]
                   [lein-pprint "1.1.2"]]}}
